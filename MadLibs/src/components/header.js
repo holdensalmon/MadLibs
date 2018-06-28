f8869cbfb89d03c432b8f7e71a4a8d45ba10426e
holdensalmon <holdensalmon@gmail.com>
2018-06-28 08:52:27 -0600

    For Jaren

---
+++ MadLibs/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>BLUE LIBS</h1>
+            <p>DONT FORGET TO SMAYASH THAT GENERATE BUTTON!!</p>
+        </div>
+    )
+}
+
+export default Header;
